ce03ba3bf377cc5460b45e758acc3365c40c6791
Sławomir Wojtasiak <slawomir.wojtasiak@fcml-lib.com>
2010-10-14 12:20:15 +0000

    git-svn-id: svn://localhost/ira/trunk@17 9740f23c-95bd-2145-a747-490f655ec538

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ira.h"
+
+int main()
+{
+
+    uint8_t data[] = {0x66, 0x36, 0xf2, 0xF3, 0x42, 0x0F, 0x6E};
+
+    struct disassemble_info info;
+    info.address = &data;
+    info.size = 2;
+    info.address_size_attribute = 32;
+    info.operand_size_attribute = 64;
+    info.mode = mod_64bit;
+
+    struct disassemble_result result;
+
+    ira_disassemble( &info, &result );
+
+    return 0;
+}
