e437f47c98edeaf591418ed797ae1ba23c14ea62
Sławomir Wojtasiak <slawomir.wojtasiak@fcml-lib.com>
2011-08-13 21:54:13 +0000

    git-svn-id: svn://localhost/ira/trunk@62 9740f23c-95bd-2145-a747-490f655ec538

--- main.c
+++ main.c
@@ -124,6 +124,7 @@ void test_code_32( int is32, uint8_t code[], int size, char *mnemonic ) {
 
 void test(void) {
 	// ADC
+
 	// 14 ib ADC AL, imm8 C Valid Valid Add with carry imm8 to AL.
 	_TEST32( "1442 adc al,42h", 0x14, 0x42 );
 	_TEST32( "1400 adc al,00h", 0x14, 0x00 );
@@ -153,5 +154,46 @@ void test(void) {
 	_TEST32( "6681d50102 adc bp,0201h", 0x66, 0x81, 0xD5, 0x01, 0x02 );
 	_TEST32( "FAIL", 0x67, 0x66, 0x40, 0x81, 0xD5, 0x01, 0x02, 0x03, 0x04 ); // 32 bit mode doesn't not allow REX.
 	_TEST64( "67664081d50102 adc bp,0201h", 0x67, 0x66, 0x40, 0x81, 0xD5, 0x01, 0x02 ); // 32 bit mode doesn't not allow REX.
+	// REX.W + 81 /2 id ADC r/m64, imm32 B Valid N.E. Add with CF imm32 sign extended to 64-bits to r/m64.
+	_TEST64( "4881d501020304 adc rbp,0000000004030201h", 0x48, 0x81, 0xD5, 0x01, 0x02, 0x03, 0x04 );
+	_TEST64( "4881d5ffffffff adc rbp,0ffffffffffffffffh", 0x48, 0x81, 0xD5, 0xff, 0xff, 0xff, 0xff );
+	_TEST64( "674881d5ffffffff adc rbp,0ffffffffffffffffh", 0x67, 0x48, 0x81, 0xD5, 0xff, 0xff, 0xff, 0xff );
+	_TEST64( "664881d5ffffffff adc rbp,0ffffffffffffffffh", 0x66, 0x48, 0x81, 0xD5, 0xff, 0xff, 0xff, 0xff );
+	// 83 /2 ib ADC r/m16, imm8 B Valid Valid Add with CF sign-extended imm8 to r/m16.
+	// 83 /2 ib ADC r/m32, imm8 B Valid Valid Add with CF sign-extended imm8 into r/m32.
+	_TEST32( "831601 adc [esi],00000001h", 0x83, 0x16, 0x01, 0x02, 0x03, 0x04 );
+	_TEST32( "66831601 adc [esi],0001h", 0x66, 0x83, 0x16, 0x01, 0x02, 0x03, 0x04 );
+	_TEST32( "678316010203 adc [0201h],00000003h", 0x67, 0x83, 0x16, 0x01, 0x02, 0x03 );
+	// REX.W + 83 /2 ib ADC r/m64, imm8 B Valid N.E. Add with CF sign-extended imm8 into r/m64.
+	_TEST64( "4883970102030405 adc [rdi+0000000004030201h],0000000000000005h", 0x48, 0x83, 0x97, 0x01, 0x02, 0x03, 0x4, 0x05 );
+	_TEST64( "48839701020304ff adc [rdi+0000000004030201h],0ffffffffffffffffh", 0x48, 0x83, 0x97, 0x01, 0x02, 0x03, 0x4, 0xff );
+	// 10 /r ADC r/m8, r8 A Valid Valid Add with carry byte register to r/m8.
+	// REX + 10 /r ADC r/m8*, r8* A Valid N.E. Add with carry byte register to r/m64.
+	_TEST32( "10a501020304 adc [ebp+04030201h],ah", 0x10, 0xa5, 0x01, 0x02, 0x03, 04 );
+	_TEST64( "4810a501020304 adc [rbp+0000000004030201h],dil", 0x48, 0x10, 0xa5, 0x01, 0x02, 0x03, 04 );
+	_TEST64( "481064a501 adc [rbp+0000000000000001h],dil", 0x48, 0x10, 0x64, 0xa5, 0x01, 0x02, 0x03, 04 );
+	// 11 /r ADC r/m16, r16 A Valid Valid Add with carry r16 to r/m16.
+	// 11 /r ADC r/m32, r32 A Valid Valid Add with CF r32 to r/m32.
+	// REX.W + 11 /r ADC r/m64, r64 A Valid N.E. Add with CF r64 to r/m64.
+	_TEST32( "11a501020304 adc [ebp+04030201h],esp", 0x11, 0xa5, 0x01, 0x02, 0x03, 04 );
+	_TEST32( "676611a50102 adc [di+0201h],sp", 0x67, 0x66, 0x11, 0xa5, 0x01, 0x02 );
+	_TEST32( "6711a50102 adc [di+0201h],esp", 0x67, 0x11, 0xa5, 0x01, 0x02 );
+	// Do sprawdzenia na lapku.
+	_TEST64( "4d11648901 adc [r9+rcx*4+0000000000000001h],r12", 0x4D, 0x11, 0x64, 0x89, 0x01 );
+	// 12 /r ADC r8, r/m8 A Valid Valid Add with carry r/m8 to byte register.
+	// REX + 12 /r ADC r8*, r/m8* A Valid N.E. Add with carry r/m64 to byte register.
+	_TEST32( "12a501020304 adc ah,[ebp+04030201h]", 0x12, 0xa5, 0x01, 0x02, 0x03, 04 );
+	_TEST64( "4812a501020304 adc dil,[rbp+0000000004030201h]", 0x48, 0x12, 0xa5, 0x01, 0x02, 0x03, 04 );
+	_TEST64( "481264a501 adc dil,[rbp+0000000000000001h]", 0x48, 0x12, 0x64, 0xa5, 0x01, 0x02, 0x03, 04 );
+	// 13 /r ADC r16, r/m16 A Valid Valid Add with carry r/m16 to r16.
+	// 13 /r ADC r32, r/m32 A Valid Valid Add with CF r/m32 to r32.
+	// REX.W + 13 /r ADC r64, r/m64 A Valid N.E. Add with CF r/m64 to r64.
+	_TEST32( "13a501020304 adc esp,[ebp+04030201h]", 0x13, 0xa5, 0x01, 0x02, 0x03, 04 );
+	_TEST32( "676613a50102 adc sp,[di+0201h]", 0x67, 0x66, 0x13, 0xa5, 0x01, 0x02 );
+	_TEST32( "6713a50102 adc esp,[di+0201h]", 0x67, 0x13, 0xa5, 0x01, 0x02 );
+	// Do sprawdzenia na lapku.
+	_TEST64( "4d13648901 adc rsp,[r9+rcx*4+0000000000000001h]", 0x4D, 0x13, 0x64, 0x89, 0x01 );
+
 }
+
 
