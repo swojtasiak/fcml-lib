f2e73647d1248ab2798f8410619bd9bff429885f
Sławomir Wojtasiak <slawomir.wojtasiak@fcml-lib.com>
2013-06-06 23:08:07 +0200

    asm

--- instructions_a_t.c
+++ instructions_a_t.c
@@ -118,6 +118,9 @@ void fcml_tf_instruction_ADC(void) {
 	FCML_I64( "adc word ptr [rsi],0001h", 0x66, 0x83, 0x16, 0x01 );
 	FCML_I32( "adc dword ptr [0201h],00000003h", 0x67, 0x83, 0x16, 0x01, 0x02, 0x03 );
 	// 00000000`00401000 8315faf1bfff03  adc     dword ptr [00000000`00000201],3
+
+	//0x401000
+	//0xFFBFF1FA
 }
 
 CU_TestInfo fctl_ti_instructions_a[] = {
