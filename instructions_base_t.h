ce268ca7491d97e98c33dabb27c6565bac177797
Sławomir Wojtasiak <slawomir.wojtasiak@fcml-lib.com>
2013-06-12 23:34:48 +0200

    rip

--- instructions_base_t.h
+++ instructions_base_t.h
@@ -22,6 +22,9 @@
 #define FCML_I3264_P(x,...) { FCML_I32_P(x,__VA_ARGS__); FCML_I64_P(x,__VA_ARGS__); }
 #define FCML_I32_P(x,...) { fcml_uint8_t code[] = {__VA_ARGS__}; IA3264_instruction_test( code, sizeof( code ), FCML_FALSE, x, FCML_TRUE, FCML_TRUE, FCML_FALSE); }
 #define FCML_I64_P(x,...) { fcml_uint8_t code[] = {__VA_ARGS__}; IA3264_instruction_test( code, sizeof( code ), FCML_TRUE, x, FCML_TRUE, FCML_TRUE, FCML_FALSE); }
+#define FCML_I3264_RIP_P(x,...) { FCML_I32_RIP_P(x,__VA_ARGS__); FCML_I64_RIP_P(x,__VA_ARGS__); }
+#define FCML_I32_RIP_P(x,...) { fcml_uint8_t code[] = {__VA_ARGS__}; IA3264_instruction_test( code, sizeof( code ), FCML_FALSE, x, FCML_TRUE, FCML_TRUE, FCML_TRUE); }
+#define FCML_I64_RIP_P(x,...) { fcml_uint8_t code[] = {__VA_ARGS__}; IA3264_instruction_test( code, sizeof( code ), FCML_TRUE, x, FCML_TRUE, FCML_TRUE, FCML_TRUE); }
 
 #define FCML_I3264_D(x,...) { FCML_I32_D(x,__VA_ARGS__); FCML_I64_D(x,__VA_ARGS__); }
 #define FCML_I32_D(x,...) { fcml_uint8_t code[] = {__VA_ARGS__}; IA3264_instruction_diss_test( code, sizeof( code ), FCML_FALSE, x, FCML_TRUE); }
