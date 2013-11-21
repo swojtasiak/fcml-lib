8349fbf4394567abf192b1156e3bbac9384a5d78
Sławomir Wojtasiak <slawomir.wojtasiak@fcml-lib.com>
2013-11-22 00:20:12 +0100

    dialect_int

---
+++ fcml_dialect.h
@@ -0,0 +1,15 @@
+/*
+ * fcml_dialect.h
+ *
+ *  Created on: Nov 21, 2013
+ *      Author: tas
+ */
+
+#ifndef FCML_DIALECT_H_
+#define FCML_DIALECT_H_
+
+// Assembler dialect.
+typedef struct fcml_st_dialect_context {
+} fcml_st_dialect_context;
+
+#endif /* FCML_DIALECT_H_ */
