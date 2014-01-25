3b8ac6d137427ab020fdcf2c7180085e342ae6c6
Sławomir Wojtasiak <slawomir.wojtasiak@fcml-lib.com>
2014-01-25 03:22:43 +0100

    Tests project prepared.

---
+++ check/main.c
@@ -0,0 +1,13 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 25, 2014
+ *      Author: tas
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	printf("Check Check Check!!\n");
+	return 0;
+}
