0dce9667368b0c5ca43267e8d4cefacdb0526981
Sławomir Wojtasiak <slawomir.wojtasiak@fcml-lib.com>
2014-01-25 14:41:29 +0100

    Valgrind scripts modified.

--- valgrind_lib.sh
+++
@@ -1,1 +0,0 @@
-LD_LIBRARY_PATH=:/home/tas/workspace_fcml/fcml/src/.libs:/home/tas/workspace_fcml/fcml/src/.libs valgrind Debug/fcml-test
--- valgrind_run.sh
+++ valgrind_run.sh
@@ -1,1 +1,2 @@
-valgrind --track-origins=yes --leak-check=full ./Debug/fcml-test.exe > valgrind-result.txt 2>&1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:src/.libs valgrind 
+valgrind --track-origins=yes --leak-check=full check/fcml-check > valgrind-result.txt 2>&1
