21b9c1c7d7ab5ee55b453038a33b79c3d737ca44
Sławomir Wojtasiak <slawomir.wojtasiak@fcml-lib.com>
2014-02-02 20:17:57 +0100

    Error handling

---
+++ check/error_handling_t.h
@@ -0,0 +1,15 @@
+/*
+ * error_handling.h
+ *
+ *  Created on: Feb 1, 2014
+ *      Author: tas
+ */
+
+#ifndef ERROR_HANDLING_H_
+#define ERROR_HANDLING_H_
+
+#include "fcml_stf.h"
+
+extern fcml_stf_test_suite fcml_si_error_handling;
+
+#endif /* ERROR_HANDLING_H_ */
