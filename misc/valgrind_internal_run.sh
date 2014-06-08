ad66d624ddecd0203538306abbd9db327a7356da
slawomir.wojtasiak <slawomir.wojtasiak@fcml-lib.com>
2014-06-08 21:16:36 +0200

    some files moved to misc

---
+++ misc/valgrind_internal_run.sh
@@ -0,0 +1,1 @@
+valgrind --track-origins=yes --leak-check=full ../check/internal-tests/fcml_internal_check > valgrind-internal-result.txt 2>&1
--- valgrind_public_run.sh
+++
@@ -1,3 +0,0 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:src/.libs
-valgrind --track-origins=yes --leak-check=full check/public-tests/.libs/fcml_public_check > valgrind-result.txt 2>&1
-
