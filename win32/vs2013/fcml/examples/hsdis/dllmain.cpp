a2d290731fad8d5fa0c85dab4b2022254116afce
slawomir.wojtasiak <slawomir.wojtasiak@fcml-lib.com>
2015-02-03 19:59:04 +0100

    some fixes related to the licenses

--- win32/vs2013/fcml/examples/hsdis/dllmain.cpp
+++ win32/vs2013/fcml/examples/hsdis/dllmain.cpp
@@ -1,6 +1,6 @@
 /**
  * FCML - Free Code Manipulation Library.
- * Copyright (C) 2014 Slawomir Wojtasiak
+ * Copyright (C) 2014-2015 Slawomir Wojtasiak
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
