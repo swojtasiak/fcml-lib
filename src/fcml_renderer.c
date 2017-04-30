acf6bda83b595ae7251e63b535aa1c18071ec1fa
Sławomir Wojtasiak <slawomir.wojtasiak@fcml-lib.com>
2017-04-30 12:06:20 +0200

    code formatting

--- src/fcml_renderer.c
+++ src/fcml_renderer.c
@@ -1,6 +1,6 @@
 /*
  * FCML - Free Code Manipulation Library.
- * Copyright (C) 2010-2015 Slawomir Wojtasiak
+ * Copyright (C) 2010-2017 Slawomir Wojtasiak
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -24,17 +24,21 @@
 #include "fcml_trace.h"
 #include "fcml_env_int.h"
 
-fcml_ceh_error LIB_CALL fcml_fn_render( const fcml_st_dialect *dialect, const fcml_st_render_config *config, fcml_char *buffer, fcml_usize buffer_len,
-        const fcml_st_disassembler_result *result ) {
+fcml_ceh_error LIB_CALL fcml_fn_render(const fcml_st_dialect *dialect,
+        const fcml_st_render_config *config, fcml_char *buffer,
+        fcml_usize buffer_len,
+        const fcml_st_disassembler_result *result) {
 
     fcml_st_memory_stream output_stream;
     output_stream.base_address = buffer;
     output_stream.offset = 0;
     output_stream.size = buffer_len;
 
-    fcml_st_dialect_context_int *dialect_context_int = (fcml_st_dialect_context_int*) dialect;
+    fcml_st_dialect_context_int *dialect_context_int =
+            (fcml_st_dialect_context_int*)dialect;
     if ( dialect_context_int->instruction_renderer ) {
-        fcml_fnp_render_instruction renderer = (fcml_fnp_render_instruction) dialect_context_int->instruction_renderer;
+        fcml_fnp_render_instruction renderer = (fcml_fnp_render_instruction)
+                dialect_context_int->instruction_renderer;
         return renderer( dialect, config, &output_stream, result );
     } else {
         /* Dialect not initialized correctly.*/
